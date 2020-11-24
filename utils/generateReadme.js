189d2693db01c7c66e62ed706f07add983ea7a32
zi wang <ziwang55@gmail.com>
2020-11-24 15:50:38 -0600

    adding generateReadme to utils js file

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,6 +1,22 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateReadme(answers) {
+  return `
+  <h1 align="center">${answers.ProjectTitle} 👋</h1>
+
+  ![badge](https://img.sheilds.io/badge/license-${answers.license}-brightgreen)<br />
+
+  ## Description
+  🔎 ${answers.description}
+
+  ## Table of Content
+  -[Description](#description)
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributing](#contributing)
+  -[Questions](#questions)
+
+  
 
 `;
 }
