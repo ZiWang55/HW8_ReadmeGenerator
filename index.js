9ca5511464fe13cd1d05c8efda1f208bd4906117
zi wang <ziwang55@gmail.com>
2020-11-24 13:09:46 -0600

    adding questions to js

--- index.js
+++ index.js
@@ -13,6 +13,34 @@ function promptUser(){
             name: "projectTitle",
             message: "Whats the name for the project title?",
         },
-        {}
+        {
+            type: "input",
+            name: "description",
+            message: "Brief description of your project: ",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process: ",
+        }
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project used for? ",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
     ])
 }
